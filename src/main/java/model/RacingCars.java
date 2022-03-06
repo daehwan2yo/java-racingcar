506a50e46572a7be7fedf8eb15c9b1d9756c4b24
mac_daehwan2yo <daehwan7989@naver.com>
2022-03-06 19:58:45 +0900

    feat -#2 - 자동차 일급 컬랙션에 대한 기능 구현, success

--- src/main/java/model/RacingCars.java
+++ src/main/java/model/RacingCars.java
@@ -1,20 +1,27 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RacingCars implements Cars {
+	private final List<Car> carList;
+	
+	public RacingCars() {
+		carList = new ArrayList<>();
+	}
+	
 	@Override
 	public void save(Car car) {
-	
+		carList.add(car);
 	}
 	
 	@Override
 	public int size() {
-		return 0;
+		return carList.size();
 	}
 	
 	@Override
 	public List<Car> getAll() {
-		return null;
+		return carList;
 	}
 }
